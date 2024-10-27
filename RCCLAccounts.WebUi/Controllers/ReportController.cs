98372f5c053a942ef09a194f909c4a2bd3a0ea23
Kazi Safiul Kadar (Jewel) <jeweldiu28@gmail.com>
2024-10-27 21:03:52 +0600

    Update Version 16

--- RCCLAccounts.WebUi/Controllers/ReportController.cs
+++ RCCLAccounts.WebUi/Controllers/ReportController.cs
@@ -32,12 +32,6 @@ namespace RCCLAccounts.WebUi.Controllers
     {
         #region ClassDeclare
 
-        public class LoanInterestDate
-        {
-            public int TransactionID { get; set; }
-            public string InterestDate { get; set; }
-        }
-
         public class LedgerList
         {
             public string LedgerId { get; set; }
@@ -45,8 +39,6 @@ namespace RCCLAccounts.WebUi.Controllers
         }
 
         #endregion
-
-
 
         private readonly ILogger<ReportController> _logger;
         private readonly IWebHostEnvironment _webHostEnvironment;
@@ -63,33 +55,21 @@ namespace RCCLAccounts.WebUi.Controllers
         string sqlCon;
         string hostUrl;
 
-
-        private readonly IEmployeeCPFOpeningService _employeeCPFOpeningService;
-        private readonly IBankAccountInfoService _bankAccountInfoService;
-        public IEmpService empService { get; set; }
-        public IBankDepositService bankservice { get; set; }
-
         AccountReportServiceWebUi service;
         public ReportController(IWebHostEnvironment webHostEnvironment,
             ILogger<ReportController> logger,     
             IHttpContextAccessor accessor,           
-            AppDbContext db,
-            IEmployeeCPFOpeningService employeeCPFOpeningService,
-            IEmpService _empService,
-            IBankAccountInfoService bankAccountInfoService,
-            IBankDepositService _bankservice)
+            AppDbContext db)
+            
         {
             _webHostEnvironment = webHostEnvironment;
             _logger = logger;       
             _accessor = accessor;
-
             _db = db;
             sqlCon = _db.Database.GetDbConnection().ConnectionString;
-            this.empService = _empService;
+       
             hostUrl = _accessor.HttpContext.Request.Scheme + "://" + _accessor.HttpContext.Request.Host;
-            _employeeCPFOpeningService = employeeCPFOpeningService;
-            _bankAccountInfoService = bankAccountInfoService;
-            this.bankservice = _bankservice;
+            
             service = new AccountReportServiceWebUi(_accessor, _db);
         }
        public IActionResult Index()
@@ -97,30 +77,11 @@ namespace RCCLAccounts.WebUi.Controllers
            
          return View();
 
-            //reportName = "EmployeeInfo.frx";
-            //_title = "Employye Information";
-
-            //var sql = "SELECT * FROM EmployeeInfos";
-
-
-            //_logger.LogInformation(sql);
-            //sqls.Clear();
-            //sqls.Add(sql);
-            //return ShowReport(0);
-
         }
         public async Task<IActionResult> PertialView(string viewName)
         {
 
-            ViewBag.Employee = new SelectList(await _employeeCPFOpeningService.GetAllEmpNoAndName(), "EmpolyeeId", "EmployeeName");
-            ViewBag.Branches = new SelectList(await empService.GetBranches(), "BranchId", "BranchName");
-            ViewBag.Bank = new SelectList(await _bankAccountInfoService.GetAllBankAsync(), "BankId", "BankNames");
-            ViewBag.LoanAccount = new SelectList(GetLoanLoad(), "LoanId", "LoanNo");
-            ViewBag.MonthlyLoanInterest = new SelectList(GetMonthlyLoanInterestDate(), "TransactionID", "InterestDate");
-
-            ViewBag.Ledger = new SelectList(GetLedgerLoad(), "LedgerId", "LedgerName");
-            ViewBag.AccountInfo = new SelectList(await bankservice.GetBankAccount(), "BankAcInfoId", "AccountNo");
-
+            ViewBag.Ledger = new SelectList(GetLedgerLoad(), "LedgerId", "LedgerName");       
             return PartialView(viewName);
 
         }
@@ -130,11 +91,8 @@ namespace RCCLAccounts.WebUi.Controllers
         public IActionResult VoucherAll(string vType, string findType, string fromDate, string toDate, string voucherNo)
         {
          
-            var companyId = "B-1";
-           
-
+            var companyId = "B-1";          
             string voucherData;
-
             string voucherType = service.getVoucherName(vType);
             voucherNo = voucherNo.Equals("All") ? "%" : voucherNo;
             if (findType == "Date")
@@ -188,55 +146,13 @@ namespace RCCLAccounts.WebUi.Controllers
                     break;
             }
 
-
             _logger.LogInformation(sql);
             sqls.Clear();
             sqls.Add(sql);
             return ShowReport(0);
         }
-        public IActionResult GetEmployeeInfo(String BranchID)
-        {
-            reportName = "EmployeeInfo.frx";
-            _title = "Employee Information";
+       
 
-            var sql = " Select e.*,b.BranchName from [dbo].[EmployeeInfos] e left outer join[dbo].[Departments] d on e.DepartmentId = d.Id left outer join[dbo].[BranchInformation] b on b.BranchId = e.BranchId left outer join[dbo].[Designations] de on de.Id = e.DesignationId where e.BranchId like '" + BranchID + "'  order by BranchId,Cast(e.EmployeeNo as int)  ";
-     
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        public IActionResult GetEmployeeDetail(String EmpolyeeId)
-        {
-            reportName = "EmployeeDetails.frx";
-            _title = "Employee Details ";
-
-            var sql = " Select e.EmpolyeeId,e.EmployeeNo,e.EmployeeName,e.FatherName,e.MotherName,e.MobileNo,e.Email, " +
-                " e.Gender,CASE WHEN e.PicturePath is NULL THEN '' WHEN e.PicturePath =''  THEN '' Else '" + hostUrl + "'+'/' + replace(e.PicturePath, '\\','/') END AS PicturePath ," +
-                " CASE WHEN e.SigniturePath is NULL THEN '' WHEN e.SigniturePath =''  THEN '' Else '" + hostUrl + "'+'/' + replace(e.SigniturePath, '\\','/') END AS SigniturePath," +
-                " e.DateOfBirth,e.JoiningDate,e.RetiredMentDate,e.PresentAddress,e.PermanentAddress, " +
-                " e.BasicSalary,e.OwnContPer,e.CompanyContPer,e.CpfStartDate,b.BranchName,de.Name as Designation,d.Name as DepartmentName,e.NID " +
-                " from  [dbo].[EmployeeInfos] e " +
-                " left outer join[dbo].[Departments] d on e.DepartmentId = d.Id" +
-                " left outer join[dbo].[BranchInformation] b on b.BranchId = e.BranchId " +
-                " left outer join[dbo].[Designations] de on de.Id = e.DesignationId " +
-                " where e.EmpolyeeId like '" + EmpolyeeId +"'  ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-
-            string sql1 = "Select e.EmpolyeeTransferId as Serial,e.BasicSalary,b.BranchName,e.PostingDate,e.LeaveDate,de.Name as Designation,d.Name as DepartmentName ,1 OrderType," +
-                "'Previous Branch' History from[dbo].[EmployeeTransferHistory] e left outer join[dbo].[Departments] d on e.DepartmentId = d.Id left outer join[dbo].[BranchInformation] " +
-                "b on b.BranchId = e.BranchId left outer join[dbo].[Designations] de on de.Id = e.DesignationId where e.TranserEmpolyeeId like '" + EmpolyeeId +"' Union Select e.EmpolyeeId as Serial," +
-                "e.BasicSalary,b.BranchName,'' PostingDate,'' LeaveDate,de.Name as Designation,d.Name as DepartmentName ,1 OrderType,'Present Branch' History from[dbo].[EmployeeInfos] e " +
-                "left outer join[dbo].[Departments] d on e.DepartmentId = d.Id left outer join[dbo].[BranchInformation] b on b.BranchId = e.BranchId left outer join[dbo].[Designations] de " +
-                "on de.Id = e.DesignationId where e.EmpolyeeId like '" + EmpolyeeId +"' order by OrderType,Serial";
-
-            sqls.Add(sql1);
-            return ShowReport(1);
-        }
         // Chart Of Account
         public IActionResult ChartOfAccount()
         {
@@ -253,11 +169,7 @@ namespace RCCLAccounts.WebUi.Controllers
         {
             reportName = "rptDebitCreditSuplementory.frx";
             _title = "Suplementory ";
-
-
             caption = VoucherType;
-
-
             var sql = "";
 
             if (VoucherType == "DEBIT")
@@ -280,180 +192,6 @@ namespace RCCLAccounts.WebUi.Controllers
             sqls.Add(sql);
             return ShowReport(0);
         }
-
-
-        public IActionResult GetMonthlyCPFStatement(String Year, String Month)
-        {
-            reportName = "rptMonthlyCPFStatement.frx";
-            _title = "Monthly CPF Statement ";
-
-
-            var sql = "Select * from [dbo].[func_MonthlyCPFStatement] ('" + Year + "','" + Month + "') order by BranchId,year,MonthId,EmpolyeeId";
-
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-        public IActionResult GetCPFLedgerBalance(String AsOnDate)
-        {
-            reportName = "rptCPFLedgerBalance.frx";
-            _title = "Branch Wise CPF Ledger Balance";
-
-
-            var sql = "SELECT * FROM [dbo].[func_CPFLedgerBalance] ('" + AsOnDate + "') ";
-
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        public IActionResult GetIndividualCPFLedger(String FromDate, String Todate, String EmpolyeeId)
-        {
-            reportName = "rptIndividualCPLedger.frx";
-            _title = "Individual CPF Ledger Report ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = " Select * from [dbo].[func_IndividualCPFLedger] (CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "') ,'" + EmpolyeeId +"' ) ";
-           
-           
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        public IActionResult GetBankWiseAccountStatement(String FromDate, String Todate, String BankId)
-        {
-            reportName = "rptBankWiseAccountStatement.frx";
-            _title = "Bank Wise Account Statement ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = "SELECT * FROM [dbo].[func_BankWiseAccountBalance](CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "') ,'" + BankId + "') ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        public IActionResult GetCPFLoanLedgerBalance(String AsOnDate)
-        {
-            reportName = "rptCPFLoanLedgerBalance.frx";
-            _title = "CPF Loan Ledger Balance";
-
-            caption = "As On Date " + dateToString(stringToDate(AsOnDate), "dd-MM-yy");
-
-            var sql = "SELECT * FROM [dbo].[func_CPFLoanLedgerBalance](CONVERT(date,'" + AsOnDate + "')) ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        public IActionResult GetIndividualCPFLoanLedger(String FromDate, String Todate, String LoanID)
-        {
-            reportName = "rptIndividualCPFLoanLedger.frx";
-            _title = "Individual CPF Loan Ledger Report ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = " Select * from [dbo].[func_IndividualCPFLoanLedger] (CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "') ,'" + LoanID + "' ) ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        // Officer Wise Monthly CPF Statement
-        public IActionResult GetOfficerWiseMonthlyCPFStatement(String Year) 
-        { 
-            reportName = "rptYearWiseCPFLBalance.frx";
-            _title = "Officer Wise Monthly CPF Statement ";
-
-            caption = "Year : " +  Year ;
-
-            // var sql = "Select * from [dbo].[func_MonthlyCPFStatement] ('" + Year + "','" + Month + "') order by BranchId,year,MonthId,EmpolyeeId";
-
-            var sql = "Exec [dbo].[sp_RptOfficerWiseYearlyCPFStatement] '" + Year + "' ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-        //
-
-        public IActionResult GetMonthlyLoanInterestPosting(String TrID)
-        {
-            reportName = "rptMonthlyLoanInterestPosting.frx";
-            _title = "Monthly Loan Interest Posting Statement ";
-
-            caption = "Date : " + TrID;
-
-            var sql = "Exec [dbo].[sp_RptMonthlyLoanInteretPosting] '" + TrID + "' ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-        // Officer Wise Individual CPF Ledger
-        public IActionResult GetOfficerWiseIndividualCPFLedger(String FromDate, String Todate, String EmpolyeeId)
-        {
-            reportName = "rptOfficerWiseIndividualCPLedger.frx";
-            _title = "Officer Wise Individual CPF Ledger Report ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = " Select * from [dbo].[func_OfficerWiseIndividualCPFStatement]  (CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "') ,'" + EmpolyeeId + "' ) ";
-
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-        //
-        public IActionResult GetOffAndBrWiseCPFStatement(String Year, String Month)
-        {
-            reportName = "rptBranchAndOfficerWiseMonthlyCPFDepositStatement.frx";
-            _title = "Officer And Branch Wise Monthly CPF Statement ";
-
-
-            var sql = "Select * from [dbo].[func_BranchAndOfficerWiseMonthlyCPFDepositStatement] ('" + Year + "','" + Month + "') Order by BranchId,YearMonth,EmpolyeeId";
-
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        // Officer & Br  Wise  CPF Profit Statement
-        public IActionResult GetOfficerAndBranchWiseCPFProfitStatement(String Year)
-        {
-            reportName = "rptBranchAndOfficerWiseMonthlyCPFProfitStatement.frx";
-            _title = "Officer & Branch Wise Profit Distribution Statement";
-
-            caption = "Year : " + Year;
-
-             var sql = "Select * from [dbo].[func_BranchAndOfficerWiseMonthlyCPFProfitStatement] ('" + Year + "') order by BranchId,Year,EmpolyeeId";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-
 
         // Trail Balance Between Date
         public IActionResult GetTrailBalanceBetweenDate(String FromDate, String Todate)
@@ -489,41 +227,7 @@ namespace RCCLAccounts.WebUi.Controllers
             sqls.Add(sql1);
             return ShowReport(1);
         }
-
-        // Individual Bank Ledger
-
-        public IActionResult GetIndividualBankLedger(String FromDate, String Todate, String BankAcInfoId)
-        {
-            reportName = "rptIndividualBankLedger.frx";
-            _title = "Individual Bank Ledger Report ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = " Select * from [dbo].[func_IndividualBankLedger] (CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "') ,'" + BankAcInfoId + "' ) ";
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
-        // Loan Transaction Date Between 
-        public IActionResult GetLoanTransactionBetweenDate(String FromDate, String Todate)
-        {
-            reportName = "rptLoanTransactionDateBetween.frx";
-            _title = "CPF Loan Transaction Date Between ";
-
-            caption = "From " + dateToString(stringToDate(FromDate), "dd-MM-yy") + " To : " + dateToString(stringToDate(Todate), "dd-MM-yy");
-
-            var sql = " Select * from [dbo].[func_CPFLoanTransactionDateBetween] (CONVERT(date,'" + FromDate + "') , CONVERT(date,'" + Todate + "')) ";
-
-
-            _logger.LogInformation(sql);
-            sqls.Clear();
-            sqls.Add(sql);
-            return ShowReport(0);
-        }
-
+  
         // Statement Of Affairs
         public IActionResult GetStatementOfAffairs(String FromDate, String Todate)
         {
@@ -605,36 +309,6 @@ namespace RCCLAccounts.WebUi.Controllers
 
         }
       
-        public List<LoanInterestDate> GetMonthlyLoanInterestDate()
-        {
-            List<LoanInterestDate> loans = new List<LoanInterestDate>();
-            SqlConnection con = new SqlConnection(sqlCon);
-            try
-            {
-                con.Open();
-                string sql = " Select Distinct TransactionId,convert(date,TransactionDate,105)TransactionDate from [dbo].[InterestPosting] order by TransactionDate asc ";
-
-                SqlCommand cmd = new SqlCommand(sql, con);
-                SqlDataReader sqlData = cmd.ExecuteReader();
-                while (sqlData.Read())
-                {
-                    loans.Add(new LoanInterestDate
-                    {
-                        TransactionID = Convert.ToInt32(sqlData["TransactionId"]),
-                        InterestDate = sqlData["TransactionDate"].ToString()
-                    });
-                }
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return loans;
-
-        }
-
-
         public List<LedgerList> GetLedgerLoad()
         {
             List<LedgerList> ledgers = new List<LedgerList>();
