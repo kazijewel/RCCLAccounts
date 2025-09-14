631b78d468617f5a7c40ec77472035dbaf1162ed
Kazi Safiul Kadar (Jewel) <jeweldiu28@gmail.com>
2025-09-14 21:01:39 +0600

    Fiscal Year Update

---
+++ RCCLAccounts.WebUi/wwwroot/assets/js/JSAccounts/FiscalYearClosingService.js
@@ -0,0 +1,45 @@
+﻿
+
+
+$(document).ready(function () {
+    loadFiscalYearData();
+});
+
+function loadFiscalYearData() {
+    $.ajax({
+        url: '/FiscalYearCloseing/getFiscalYearDate',
+        type: 'GET',
+        dataType: 'json',
+        success: function (res) {
+            if (res && res.Opening && res.Closing) {
+                const fiscalRange = res.Opening + ' to ' + res.Closing;
+                updateFiscalYearUI(fiscalRange);
+            } else {
+                showDefaultFiscalYear();
+            }
+        },
+        error: function () {
+            showDefaultFiscalYear();
+        }
+    });
+}
+
+// Helper methods
+function updateFiscalYearUI(fiscalYearData) {
+    $('#currentFiscalYearDisplay').text(fiscalYearData);
+    $('#periodText').text('Period: ' + fiscalYearData);
+}
+
+function showDefaultFiscalYear() {
+    $('#currentFiscalYearDisplay').text('2024-07-01 to 2025-06-30');
+    $('#periodText').text('Period: 2024-07-01 to 2025-06-30');
+}
+
+
+
+
+
+
+
+
+
